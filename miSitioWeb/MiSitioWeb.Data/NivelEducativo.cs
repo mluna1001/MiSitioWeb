8f754271db8b735db41407d85198a0cfde7ddafd
Marco Antonio Luna Serrano <mluna1001@gmail.com>
2018-03-30 13:46:47 -0600

    First commint
    Va

---
+++ miSitioWeb/MiSitioWeb.Data/NivelEducativo.cs
@@ -0,0 +1,19 @@
+﻿namespace MiSitioWeb.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class NivelEducativo
+    {
+        [Key]
+        public int IdNivelEstudio { get; set; }
+
+        [Display(Name = "Nivel de Estudios")]
+        public string Nombre { get; set; }
+
+    }
+}
